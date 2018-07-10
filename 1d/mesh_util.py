7416625e28bf02b46c42e8fef9002ef9da96d007
Dow Drake <dowdrake@msn.com>
2018-07-09 17:32:04 -0700

    more 1d work

--- 1d/mesh_util.py
+++ 1d/mesh_util.py
@@ -1,6 +1,6 @@
 from netgen.meshing import *
 
-def uniform_1d_mesh(a=0,b=1,nel=10, material='material'):
+def uniform_1d_mesh(a=0,b=1,nel=10, material='material', periodic=False):
     """ returns a mesh of interval (a,b) with nel elements
     """
     m = Mesh()
@@ -11,17 +11,19 @@ def uniform_1d_mesh(a=0,b=1,nel=10, material='material'):
         pnums.append(m.Add (MeshPoint(Pnt(a+(b-a)*i/nel, 0, 0))))
 
     for i in range(nel):
-        m.Add (Element1D([pnums[i], pnums[i+1]]))
+        m.Add (Element1D([pnums[i], pnums[i+1]],index=1))
 
     m.SetMaterial(1,'material')
 
     # add points
     m.Add (Element0D (pnums[0], index=1))
     m.Add (Element0D (pnums[nel], index=2))
-
-    # set boundary condition names
-    m.SetBCName(0,'left')
-    m.SetBCName(1,'right')
-
+    if periodic:
+        m.AddPointIdentification(pnums[0],pnums[nel],1,2)
+    else:
+        # set boundary condition names
+        m.SetBCName(0,'left')
+        m.SetBCName(1,'right')
     return m
+
 
